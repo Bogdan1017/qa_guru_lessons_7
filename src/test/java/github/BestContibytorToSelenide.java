ea9b67fab698c4380df4b2df2d73824a18485e87
Bohdan Korolchuck <korolchuckby@gmail.com>
2024-10-15 09:31:54 +0300

    Second test - Done

--- src/test/java/github/BestContibytorToSelenide.java
+++ src/test/java/github/BestContibytorToSelenide.java
@@ -1,9 +1,12 @@
 package github;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,6 +20,8 @@ public class BestContibytorToSelenide {
         $("div.Layout-sidebar").$(byText("Contributors"))
                 .closest(".BorderGrid-cell").$("ul li").hover();
 //Проверка: В всплывающем окне есть текст Andrei Solntsev
+        $$(".Popover").findBy(visible).shouldHave(text("Andrei Solntsev"));
+
         sleep(5000);
     }
 
